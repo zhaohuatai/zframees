e514aae89ce5d5ca3e5d32e4a0f3a595a9d168f5
zhaohuatai <824069438@qq.com>
2015-05-09 01:20:58 +0800

    add spring mail

--- src/com/zht/demo/mail/SendmailTest.java
+++ src/com/zht/demo/mail/SendmailTest.java
@@ -9,31 +9,36 @@ import org.zht.framework.spring.SpringUtils;
 
 
 public class SendmailTest {
-	@Resource 
-	private SpringMailManger  springMailManger;
 	
 	 public static void main( String[] args ) { 
-		  ConfigurableApplicationContext context= new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		 ConfigurableApplicationContext context= new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
 		  SpringMailManger springMailManger = context.getBean(SpringMailManger.class);
-//		  SpringMailManger springMailManger = SpringUtils.getBean(SpringMailManger.class);
 		  String userName="972855736@qq.com";
-		  String url= "account/forgotPassword.html?act=confirm&key=" 
-							+ "KEY" + "&account=" + "972855736@qq.com" + "824069438@qq.com";
+		  String url= "http://wwww.zhaohuatai.com/account/forgotPassword.html";
 		  String siteName="zhaohuatai";
 		  String siteUrl="http://www.zhaohuatai.com";
-		  String activeDays="3";
-		  String mailContent = SpringMailManger.loadMailTemplate("forgotPassword");
-		  //E:\workspace\myeclipse2015\zframees\WebRoot\WEB-INF\classes\template\mail
-		 mailContent=mailContent.replaceAll("\\#\\{siteName\\}",userName )
-					.replaceAll("\\#\\{url\\}", siteUrl 
-							+ "account/forgotPassword.html?act=confirm&key=" 
-							+ "KEY" + "&account=" + "972855736@qq.com")
-							.replace("\\#\\{activeDays\\}", activeDays)
-							.replace("\\#\\{siteUrl\\}", siteUrl)
-							.replace("\\#\\{siteName\\}", siteName);
-			
+		  String activeTime="3";
+		  String activeTimeUnit="天";
+		String mailContent = SpringMailManger.loadMailTemplate("forgotPassword");
+		mailContent = mailContent.replace("$userAccount", userName);
+		mailContent = mailContent.replace("$url", url);
+		mailContent = mailContent.replace("$activeTime", activeTime);
+		mailContent = mailContent.replace("$timeUnit", activeTimeUnit);
+		mailContent = mailContent.replace("$siteName",siteName );
+		mailContent = mailContent.replace("$siteUrl", siteUrl);
 			// 发送修改密码邮件
-		  springMailManger.sendMimeEmail(mailContent, "重置密码", "972855736@qq.com");;
-			
+		springMailManger.sendMimeEmail(mailContent, "重置密码", "972855736@qq.com");
+		System.out.println("forgotPassword---over"); 
+		//-----------------------------------------------------------------------------
+		mailContent="";
+		mailContent = SpringMailManger.loadMailTemplate("accountActive");
+		mailContent = mailContent.replace("$userAccount", userName);
+		mailContent = mailContent.replace("$activeUrl", url);
+		mailContent = mailContent.replace("$activeTime", activeTime);
+		mailContent = mailContent.replace("$timeUnit", activeTimeUnit);
+		mailContent = mailContent.replace("$siteName", siteName );
+		mailContent = mailContent.replace("$siteUrl", siteUrl);
+		springMailManger.sendMimeEmail(mailContent, "账户激活", "972855736@qq.com");
+		System.out.println("accountActive---over"); 
 	 }
 }
